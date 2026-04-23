bfd41b7b42b5038c624b6527fc9c270165e2fee8
Harshvardhan Patil <harsh2504patil@gmail.com>
2026-04-23 16:15:09 +0530

    Phase 1 completed: Basic password unit tests done

---
+++ PasswordValidator.Unit.Tests/PasswordValidator.Tests.cs
@@ -0,0 +1,95 @@
+﻿namespace PasswordValidator.Unit.Tests;
+
+public class PasswordValidatorTests
+{
+    private readonly PasswordValidator _passwordValidator;
+
+    
+    public PasswordValidatorTests()
+    {
+            _passwordValidator = new PasswordValidator();
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    public void Validate_WhenPasswordIsNullOrWhitespace_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "PASSWORD_NULL_OR_WHITESPACE");
+    }
+
+    [Theory]
+    [InlineData("Abc123")]
+    public void Validate_WhenPasswordLengthIsLessThan8_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "PASSWORD_TOO_SHORT");
+    }
+
+    [Theory]
+    [InlineData("ascabcasc")]
+    public void Validate_WhenPasswordNotContainsUppercaseLetter_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "MISSING_UPPERCASE_LETTER");
+    }
+
+    [Theory]
+    [InlineData("ASKCJBAELSJ")]
+    public void Validate_WhenPasswordNotContainsLowercaseLetter_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "MISSING_LOWERCASE_LETTER");
+    }
+
+    [Theory]
+    [InlineData("Abcdefghs")]
+    public void Validate_WhenPasswordNotContainsDigit_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "MISSING_DIGIT");
+    }
+
+    [Theory]
+    [InlineData("Abcdefgh1")]
+    public void Validate_WhenPasswordNotContainsSpecialCharacter_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "MISSING_SPECIAL_CHARACTER");
+    }
+
+    [Theory]
+    [InlineData("Abcdefgh1  !")]
+    public void Validate_WhenPasswordHasSpacings_ThenReturnFalse(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.False(result.IsValid);
+        Assert.Contains(result.Violations,
+            v => v.RuleCode == "PASSWORD_CONTAINS_SPACING");
+    }
+
+    [Theory]
+    [InlineData("Password1234")]
+    public void Validate_WhenPasswordIsValid_ThenReturnTrue(string password)
+    {
+        var result = _passwordValidator.Validate(password);
+        Assert.True(result.IsValid);
+        Assert.Empty(result.Violations);
+    }
+}
+
--- PasswordValidator/PasswordValidator.cs
+++ PasswordValidator/PasswordValidator.cs
@@ -1,11 +1,124 @@
-﻿namespace PasswordValidator;
+﻿using PasswordValidator.Models;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+
+namespace PasswordValidator;
 
 public class PasswordValidator
 {
-    public void Validate(string password)
+    private readonly List<PasswordRuleViolation> violations = new List<PasswordRuleViolation>();
+
+    public PasswordValidationResult Validate(string password)
     {
-        // evaluate rules
-        // collect violations
-        // compute validity & strength
+        if (CheckNullOrWhitespace(password, violations) || CheckPasswordSpaces(password, violations))
+        {
+            return new PasswordValidationResult
+            {
+                IsValid = false,
+                Violations = violations
+            };
+        }
+        CheckLength(password, violations);
+
+        CheckUppercaseLetter(password, violations);
+        CheckLowercaseLetter(password, violations);
+        CheckDigit(password, violations);
+        CheckSpecialCharacter(password, violations);
+        return new PasswordValidationResult
+        {
+            IsValid = violations.Count == 0,
+            Violations = violations
+        };
+    }
+
+    private bool CheckNullOrWhitespace(string password, List<PasswordRuleViolation> violations)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "PASSWORD_NULL_OR_WHITESPACE",
+                Message = "Password cannot be null, empty, or whitespace."
+            });
+            return true;
+        }
+        return false;
+    }
+
+    private void CheckLength(string password, List<PasswordRuleViolation> violations)
+    {
+        if (password.Length < 8)
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "PASSWORD_TOO_SHORT",
+                Message = "Password must be at least 8 characters long."
+            });
+        }
+    }
+
+    private void CheckUppercaseLetter(string password, List<PasswordRuleViolation> violations)
+    {
+        if (!password.Any(char.IsUpper))
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "MISSING_UPPERCASE_LETTER",
+                Message = "Password must contain at least one uppercase letter."
+            });
+        }
+    }
+
+    private void CheckLowercaseLetter(string password, List<PasswordRuleViolation> violations)
+    {
+        if (!password.Any(char.IsLower))
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "MISSING_LOWERCASE_LETTER",
+                Message = "Password must contain at least one lowercase letter."
+            });
+        }
+    }
+
+    private void CheckDigit(string password, List<PasswordRuleViolation> violations)
+    {
+        if (!password.Any(char.IsDigit))
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "MISSING_DIGIT",
+                Message = "Password must contain at least one digit."
+            });
+        }
+    }
+
+    private void CheckSpecialCharacter(string password, List<PasswordRuleViolation> violations)
+    {
+        var regex = @"[!@#$%^&*\(\)\-_+=\{\}\[\]:;""'<>,\.?/\\|~]";
+
+        if (!Regex.IsMatch(password, regex))
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "MISSING_SPECIAL_CHARACTER",
+                Message = "Password must contain at least one special character."
+            });
+        }
+    }
+    private bool CheckPasswordSpaces(string password, List<PasswordRuleViolation> violations)
+    {
+
+        if (password.Any(char.IsWhiteSpace))
+        {
+            violations.Add(new PasswordRuleViolation
+            {
+                RuleCode = "PASSWORD_CONTAINS_SPACING",
+                Message = "Password cannot contain spaces."
+            });
+            return true;
+        }
+        return false;
     }
 }
